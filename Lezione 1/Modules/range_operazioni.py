262f052f098e8c734d18fa1fdc41a42928d57e62
Andrea Piombo <piombacciaio@gmail.com>
2025-09-19 12:31:09 +0200

    Add additional print statements to display intermediate values of x and y

--- Lezione 1/Modules/range_operazioni.py
+++ Lezione 1/Modules/range_operazioni.py
@@ -5,6 +5,8 @@ def main():
     for _ in range(6):
         x = x + 1
         y += 1
+        print(f"Valore di x: {x}")
+        print(f"Valore di y: {y}")
     print(f"Valore finale di x: {x}")
     print(f"Valore finale di y: {y}")
 
